a200af0f691ea8f6a0ea6a0e51a10f5ec2ab7a39
sakhawat777 <biplobbms@gmail.com>
2021-11-13 11:04:20 +0600

    Problem 3 updated

--- problem3.js
+++ problem3.js
@@ -3,3 +3,11 @@ console.log(typed); // number
 
 const emtArray = typeof [];
 console.log(emtArray); // object
+
+const emtObject = {};
+console.log(typeof emtObject); // object
+
+const number = isNaN(Number.NaN);
+console.log(number); // true
+console.log(Number.NaN === NaN); // false
+console.log(Number.NaN == NaN); // false
