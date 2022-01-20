76a87e6e8a2320c5be2f402cb2b872db67fb9652
sakhawat777 <biplobbms@gmail.com>
2022-01-20 17:50:17 +0600

    SASS Added

---
+++ SAAS.js
@@ -0,0 +1,16 @@
+// What is SASS?
+// Ans: SASS is a most mature, stable and powerful professional grade css extension language in the world.
+
+// What is Sass?
+// Sass stands for Syntactically Awesome Stylesheet
+// Sass is an extension to CSS
+// Sass is a CSS pre-processor
+// Sass is completely compatible with all versions of CSS
+// Sass reduces repetition of CSS and therefore saves time
+// Sass was designed by Hampton Catlin and developed by Natalie Weizenbaum in 2006
+// Sass is free to download and use
+
+// Why Use Sass?
+// Stylesheets are getting larger, more complex, and harder to maintain. This is where a CSS pre-processor can help.
+
+// Sass lets you use features that do not exist in CSS, like variables, nested rules, mixins, imports, inheritance, built-in functions, and other stuff.
