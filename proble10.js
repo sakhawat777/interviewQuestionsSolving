d6c556e64b48ebfd8f7ff9e7161a32606e2e6909
sakhawat777 <biplobbms@gmail.com>
2022-02-23 00:48:24 +0600

    Problem 10 Added

---
+++ proble10.js
@@ -0,0 +1,3 @@
+// Undefine vs void in TypeScript?
+// undefine: When nothing return in js function that's called return undefine. Undefine is a value.
+// Void: When nothing return in TypeScript that's mean return void. Void means return no value or the value is absent.
