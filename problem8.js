407c4508946739b606a3c3c60fe43e17ce65448d
sakhawat777 <biplobbms@gmail.com>
2022-02-23 00:06:45 +0600

    Problem 8 added

---
+++ problem8.js
@@ -0,0 +1,5 @@
+// Javascript call() & apply() vs bind()?
+
+// 1. Call invokes the function and allows you to pass in arguments one by one.
+// 2. Apply invokes the function and allows you to pass in arguments as an array.
+// 3. Bind returns a new function, allowing you to pass in a this array and any number of arguments.
