698c19a137b627777081f26b7fda8fa3b5aecdb2
sakhawat777 <biplobbms@gmail.com>
2022-04-03 13:59:57 +0600

    Added Crud details

---
+++ Node-Express-Mongo-questions.js
@@ -0,0 +1,14 @@
+// What is CRUD?
+
+// Create, Read, Update, and Delete (CRUD) are the four basic functions that models should be able to do, at most.
+// The four basic operations (create, read, update, and delete) of data storage, regarded collectively.
+// "we'll create a simple database on which we will be performing CRUD operations"
+
+// CRUD Operations in Rest API
+
+// CRUD 	               HTTP	     REST
+// Create	               POST	     /api/movie
+// Read	                   GET	     /api/movie/{id}
+// Update/Replace	       PUT	     /api/movie
+// Update/Modify	       PATCH	 /api/movie
+// Delete	               DELETE	 /api/movie/{id}
