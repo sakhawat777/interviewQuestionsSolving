d7a8c154ff679911022dbb7d6ec84213b5bcf36b
sakhawat777 <biplobbms@gmail.com>
2022-04-03 14:07:47 +0600

    Updated

--- question12.js
+++ question12.js
@@ -1,3 +1,3 @@
 // Primitive vs Reference Data Types in JavaScript?
 // The Primitive Data types in JavaScript include Number, String, Boolean, Undefined, Null and Symbol.
-// Reference data types are arrays, functions, collections, and all other types of objects.
+// Reference data types are arrays, functions, collections, and all other types of objects. like date
