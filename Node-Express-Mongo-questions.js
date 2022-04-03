8835d156017c5c0a3c8764e4db161281b3a155b2
sakhawat777 <biplobbms@gmail.com>
2022-04-03 14:16:35 +0600

    database cluster added

--- Node-Express-Mongo-questions.js
+++ Node-Express-Mongo-questions.js
@@ -1,10 +1,10 @@
-// What is CRUD?
+//1. What is CRUD?
 
 // Create, Read, Update, and Delete (CRUD) are the four basic functions that models should be able to do, at most.
 // The four basic operations (create, read, update, and delete) of data storage, regarded collectively.
 // "we'll create a simple database on which we will be performing CRUD operations"
 
-// CRUD Operations in Rest API
+//2. CRUD Operations in Rest API
 
 // CRUD 	               HTTP	     REST
 // Create	               POST	     /api/movie
@@ -12,3 +12,6 @@
 // Update/Replace	       PUT	     /api/movie
 // Update/Modify	       PATCH	 /api/movie
 // Delete	               DELETE	 /api/movie/{id}
+
+// 3. what is database cluster?
+// A database cluster is a collection of databases that is managed by a single instance of a running database server. After initialization, a database cluster will contain a database named postgres, which is meant as a default database for use by utilities, users and third party applications.
