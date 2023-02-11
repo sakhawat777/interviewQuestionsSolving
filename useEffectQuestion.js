3f64f8a84d963e9a75f2280a5dcf9a4a027a14ef
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-02-11 18:09:46 +0600

    Update useEffectQuestion.js

--- useEffectQuestion.js
+++ useEffectQuestion.js
@@ -1,4 +1,4 @@
-// if i will pass empty in useeffect dependency so what happens
+// if i will pass empty {} in useeffect dependency so what happens
 // Ans:It will run the program but get Warning: useEffect received a final argument that is not an array (instead, received `object`). When specified, the final argument must be an array.
 
 // For example:
