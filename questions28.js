c508f1a9f114833d849bc9a425fc634d989888be
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-02-14 02:59:48 +0600

    Create questions28.js

---
+++ questions28.js
@@ -0,0 +1,15 @@
+28# একটি div এলিমেন্টকে center align করার সঠিক উপায় কোনটি?
+CSS Flexbox এর মাধ্যমে কোন একটি div এলিমেন্টকে horizontally এবং vertically center align করার সঠিক উপায় কোনটি?
+
+1.	কন্টেইনার এলিমেন্টটির style কে align: 'center' করে দেয়া
+
+2.	কন্টেইনার এলিমেন্টটির style কে text-align: 'center' করে দেয়া
+
+3.	কন্টেইনার এলিমেন্টটির style কে  display: flex, justify-content: 'center', align-items: 'center' করে দেয়া
+
+
+4.	কন্টেইনার এলিমেন্টটির style কে display: flex, text-alignment: 'center' করে দেয়া
+
+Ans: 3
+
+3. কন্টেইনার এলিমেন্টটির style কে display: flex, justify-content: 'center', align-items: 'center' করে দেয়া। এটি ডিভাইসের প্রান্তের কেন্দ্রে হিসেবে vertically এবং horizontally center align করে।
