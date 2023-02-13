088d91dff0ec6c59c6c28be42a7f3d3fc94857fd
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-02-14 01:11:44 +0600

    Create questions19.js

---
+++ questions19.js
@@ -0,0 +1,9 @@
+git রিপোসিটরিতে বর্তমান ব্রাঞ্চের কোড নিয়ে নতুন ব্রাঞ্চ ক্রিয়েট করার উপায় কি?
+একটি গিট রিপোসিটরির কোন একটি ব্রাঞ্চ এ থাকা অবস্থায় সেই ব্রাঞ্চের কোড নিয়ে নতুন একটি ব্রাঞ্চ ওপেন করার উপায় কি?
+
+Ans: 1
+
+1.	git checkout –b <BRANCH_NAME>
+2.	git checkout  <BRANCH_NAME>
+3.	git checkout –c <BRANCH_NAME>
+4.	git switch <BRANCH_NAME>
