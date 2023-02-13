d8f7d063553da3978ffbd10183fd177c43ab6642
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-02-14 01:18:49 +0600

    Create questions20.js

---
+++ questions20.js
@@ -0,0 +1,10 @@
+20# জাভাস্ক্রিপ্টে falsy ভ্যালু কোন গুলো?
+জাভাস্ক্রিপ্টের সকল falsy ভ্যালু গুলো সিলেক্ট করুন। এমন অপশন সিলেক্ট করবেন যেখানে সবগুলো falsy ভ্যালু আছে।
+
+Ans: 4
+
+1.	undefined, null, 0
+2.	undefined, 0, null, ‘ ’
+3.	‘ ’,  0, null, undefined, false
+4.	‘ ’,  0, null, undefined, NaN, false
+
